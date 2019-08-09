f1a6c4a90825d108eecc406c071b930a78e6bacc
Cord Walsh <cordwalsh@Cords-MacBook-Air.local>
2019-08-09 15:27:47 -0700

    can add products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,5 +10,14 @@ class ProductsController < ApplicationController
     render :new
   end
 
+  def create
+    name = params[:name]
+    product = Product.create!(:name => name)
+    redirect_to '/products' 
+  end
 
+  def show
+    @product = Product.find(params[:id])
+    render :show
+  end
 end
