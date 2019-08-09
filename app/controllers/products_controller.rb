99fe374d0a8149bd69ca0cd6e4cea689e5a9c87d
Cord Walsh <cordwalsh@Cords-MacBook-Air.local>
2019-08-09 15:11:49 -0700

    deleted foreign key, working now

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,14 @@
+class ProductsController < ApplicationController
+
+  def index
+    @products = Product.all
+    render :index
+  end
+
+  def new
+    @product = Product.new
+    render :new
+  end
+
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -24,6 +24,11 @@ ActiveRecord::Schema.define(version: 2019_08_09_211514) do
   end
 
   create_table "reviews", force: :cascade do |t|
+    t.string "author"
+    t.string "content_body"
+    t.integer "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
