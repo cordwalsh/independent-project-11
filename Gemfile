31c7d98a2dc4c6d779f4b9c4180b3e946ddb663c
Cord Walsh <cordwalsh@Cords-MacBook-Air.local>
2019-08-16 09:42:45 -0700

    seed products, working on reviews

--- Gemfile
+++ Gemfile
@@ -3,8 +3,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.5.1'
 
-gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'master'
-
+gem 'faker' 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.3'
 # Use postgresql as the database for Active Record
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,18 @@
 require 'faker'
 
-Faker::Name.name  
+50.times do
+  product = Product.create!(
+    :name => Faker::Food.dish,
+    :description => Faker::Food.description,
+    :cost => rand(5..300),
+    :country_of_origin => Faker::Address.country
+  )
+end
+
+250.times do
+  review = Review.create!(
+    :author => Faker::Name.name,
+    :content_body =>
+    :rating =>
+  )
+end
